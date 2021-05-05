81f3c1cb9b67da24c29ae29537b928d797f4572b
alex <fancn21th@aliyun.com>
2021-05-05 13:43:52 +0800

    protect all post apis

--- routes/post.route.js
+++ routes/post.route.js
@@ -2,17 +2,19 @@ const express = require("express");
 
 const postController = require("../controllers/post.controller");
 
+const protect = require("../middleware/auth.middleware");
+
 const router = express.Router();
 
 router
   .route("/")
-  .get(postController.getAllPosts)
-  .post(postController.createPost);
+  .get(protect, postController.getAllPosts)
+  .post(protect, postController.createPost);
 
 router
   .route("/:id")
-  .get(postController.getOnePost)
-  .patch(postController.updatePost)
-  .delete(postController.deletePost);
+  .get(protect, postController.getOnePost)
+  .patch(protect, postController.updatePost)
+  .delete(protect, postController.deletePost);
 
 module.exports = router;
