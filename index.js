b1999d1251db83ad61cb954b42631888e71dc2d8
alex <fancn21th@aliyun.com>
2021-05-05 08:33:54 +0800

    mongoose introduced

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+mongoose
+  .connect("mongodb://alexfan:fancn21th@mongo:27017?authSource=admin", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("successfully connected to DB");
+  })
+  .catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!!");
